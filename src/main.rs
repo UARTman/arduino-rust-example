1f4e8b380970b2eefc5c50ca75e3435d694ef393
Anton Gusev <uartman@mail.ru>
2021-02-17 15:34:12 +0300

    remove comments in Russian

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
 #![no_std]
 #![no_main]
-// Эти две директивы уберут стандартную библиотеку Rust
 
-// Подключим библиотеку - обработчик паник
 extern crate panic_halt;
 
-// Подключим библиотеку для работы с Arduino Uno
 use arduino_uno::prelude::*;
 
-// Пометим функцию main
-// Заметим, что она возвращает странный тип "!"
-// Это гарантия компилятору, что при нормальной работе она никогда не завершится
 #[arduino_uno::entry]
 fn main() -> ! {
-    // Подключим периферию
     let dp = arduino_uno::Peripherals::take().unwrap();
 
-    // Объявим интерфейс для работы с пинами
     let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
 
-    // Светодиод на пине 13. Возьмем его в режиме вывода.
     let mut led = pins.d13.into_output(&mut pins.ddr);
 
 
